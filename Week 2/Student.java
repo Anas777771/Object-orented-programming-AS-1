e2bba44bddbab5c15c999afcdd6c64ca3a0a3c03
Anas777771 <anista637@gmail.com>
2026-08-19 04:56:12 +0000

    Fix pack error and run Main

---
+++ Week 2/Student.java
@@ -0,0 +1,35 @@
+package Week 2; public class Student {
+
+    // Attributes
+    String name;
+    int age;
+    double gpa;
+
+    // Constructor
+    public Student(String studentName, int studentAge, double studentGpa) {
+
+        name = studentName;
+        age = studentAge;
+        gpa = studentGpa;
+    }
+
+    // Display student information
+    public void displayInfo() {
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+    }
+
+    // Another method
+    public void study() {
+
+        System.out.println(name + " is studying.");
+    }
+
+    // Yet another method
+    public void takeExam() {
+
+        System.out.println(name + " is taking an exam.");
+    }
+}
